a97ea73c8513ca247516e66d3dc467baaadb255a
Snail iOS <1170989734@qq.com>
2016-10-18 18:47:16 +0800

    提示框

--- CommonFrame/CommonFrame/RootViewController.m
+++ CommonFrame/CommonFrame/RootViewController.m
@@ -8,6 +8,7 @@
 
 #import "RootViewController.h"
 #import "KeyboardFitScrollView.h"
+#import "CustomHUD.h"
 @interface RootViewController ()
 
 @end
@@ -17,34 +18,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    KeyboardFitScrollView *scrollView = [[KeyboardFitScrollView alloc] initWithFrame:self.view.frame];
-    scrollView.backgroundColor = [UIColor grayColor];
-    scrollView.contentSize = CGSizeMake(0, 0);
-    
-    UITextField *textFieldOne=[[UITextField alloc] initWithFrame:CGRectMake(10, 200, 300, 50)];
-    textFieldOne.backgroundColor = [UIColor orangeColor];
-    [scrollView addSubview:textFieldOne];
-    
-    UITextField *textFieldTwo=[[UITextField alloc] initWithFrame:CGRectMake(10, 400, 300, 50)];
-    textFieldTwo.backgroundColor = [UIColor orangeColor];
-    [scrollView addSubview:textFieldTwo];
-    
-    UITextField *textFieldThree=[[UITextField alloc] initWithFrame:CGRectMake(10, 500, 300, 50)];
-    textFieldThree.backgroundColor=[UIColor orangeColor];
-    [scrollView addSubview:textFieldThree];
+//    KeyboardFitScrollView *scrollView = [[KeyboardFitScrollView alloc] initWithFrame:self.view.frame];
+//    scrollView.backgroundColor = [UIColor grayColor];
+//    scrollView.contentSize = CGSizeMake(0, 0);
+//    
+//    UITextField *textFieldOne=[[UITextField alloc] initWithFrame:CGRectMake(10, 200, 300, 50)];
+//    textFieldOne.backgroundColor = [UIColor orangeColor];
+//    [scrollView addSubview:textFieldOne];
+//    
+//    UITextField *textFieldTwo=[[UITextField alloc] initWithFrame:CGRectMake(10, 400, 300, 50)];
+//    textFieldTwo.backgroundColor = [UIColor orangeColor];
+//    [scrollView addSubview:textFieldTwo];
+//    
+//    UITextField *textFieldThree=[[UITextField alloc] initWithFrame:CGRectMake(10, 500, 300, 50)];
+//    textFieldThree.backgroundColor=[UIColor orangeColor];
+//    [scrollView addSubview:textFieldThree];
+//    
+//    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+//    button.frame = CGRectMake(10, 600, 300, 50);
+//    button.backgroundColor = [UIColor redColor];
+//    [scrollView addSubview:button];
+//    
+//    [self.view addSubview:scrollView];
+//    
+//    [scrollView autoContentSizeWithLastView:button isNeedPopkeybouard:YES];
+
     
     UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
-    button.frame = CGRectMake(10, 600, 300, 50);
-    button.backgroundColor = [UIColor redColor];
-    [scrollView addSubview:button];
-    
-    [self.view addSubview:scrollView];
-    
-    [scrollView autoContentSizeWithLastView:button isNeedPopkeybouard:YES];
-
+    [button setTitle:@"点击" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
+    button.titleLabel.font = [UIFont systemFontOfSize:15];
+    [button addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
+    button.frame = CGRectMake(0, 0, 60, 60);
+    button.center = self.view.center;
+    [self.view addSubview:button];
     // Do any additional setup after loading the view.
 }
 
+- (void)click{
+    //[CustomHUD showWithStatus:@"提示"];
+    //[CustomHUD showIndicator];
+    //[CustomHUD showIndicatorWithStatus:@"提示"];
+    //[CustomHUD showProgressWithStatus:@"提示"];
+    //[CustomHUD showPhysicsProgressWithStatus:@"提示"];
+    
+    //[CustomHUD showSuccessWithStatus:@"提示"];
+    
+    [CustomHUD showErrorWithStatus:@"提示"];
+    [CustomHUD dismissAfterTime:2.0];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
